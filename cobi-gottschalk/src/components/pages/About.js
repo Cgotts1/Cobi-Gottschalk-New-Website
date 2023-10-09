7666dff4ec9f9ea20ebea29c769a38f908565751
Cgotts1 <cobi.gottschalk@gmail.com>
2023-10-08 19:12:00 -0700

    Made slight changes to leftside

--- cobi-gottschalk/src/components/pages/About.js
+++ cobi-gottschalk/src/components/pages/About.js
@@ -4,9 +4,7 @@ import image5 from "../../images/image5.jpg";
 export default function About() {
   return (
     <div className="about-component fadeIn">
-
-
-{/* <h2>Hello there!</h2> */}
+      {/* <h2>Hello there!</h2> */}
 
       {/* <section>
         <p>
@@ -37,22 +35,41 @@ export default function About() {
         </p>
       </section> */}
 
-<section className="left-section-one">
+      <section className="left-section-one">
 
-<p style={{padding: "20px"}}>
-          With a Master of Science degree in forensic anthropology from
-          Liverpool John Moores University and a Bachelor of Arts degree in
-          anthropology from California State University, Northridge, my
-          educational background in anthropology is well-rounded. I have
-          expertise in both qualitative and quantitative research methods.
-          Furthermore, I have expanded my skill set through a comprehensive full
-          stack coding bootcamp at UCSD, where I gained practical experience and
-          sharpened my abilities in software development and web applications.
+
+        <section className="left-inside-section-one">
+        <p style={{ padding: "20px" }}>
+          {/* "I hold a Master of Science in forensic anthropology from Liverpool
+          John Moores University and a Bachelor of Arts in anthropology from
+          California State University, Northridge. My expertise spans
+          qualitative and quantitative research methods, and I've honed my
+          software development skills through a comprehensive full-stack coding
+          bootcamp at UCSD. With experience in building responsive,
+          user-friendly web applications and proficiency in programming
+          languages like HTML, CSS, and JavaScript, I offer a versatile skill
+          set that bridges anthropology and software development for a wide
+          range of roles." */}
         </p>
-</section>
-<section className="right-section-one">
+        </section>
 
-<p style={{color: "white", padding: "20px"}}>
+        <section className="right-inside-section-one">
+        <p style={{ padding: "20px" }}>
+          {/* "I hold a Master of Science in forensic anthropology from Liverpool
+          John Moores University and a Bachelor of Arts in anthropology from
+          California State University, Northridge. My expertise spans
+          qualitative and quantitative research methods, and I've honed my
+          software development skills through a comprehensive full-stack coding
+          bootcamp at UCSD. With experience in building responsive,
+          user-friendly web applications and proficiency in programming
+          languages like HTML, CSS, and JavaScript, I offer a versatile skill
+          set that bridges anthropology and software development for a wide
+          range of roles." */}
+        </p>
+        </section>
+      </section>
+      <section className="right-section-one">
+        <p style={{ color: "white", padding: "20px" }}>
           During my time at the bootcamp, I learned how to build responsive and
           user-friendly web applications, as well as how to collaborate with
           other developers using Git version control. Drawing from my previous
@@ -67,10 +84,7 @@ export default function About() {
           software development make me a well-rounded and versatile candidate
           for a variety of roles in both fields.
         </p>
-</section>
-
-
-
+      </section>
     </div>
   );
 }
